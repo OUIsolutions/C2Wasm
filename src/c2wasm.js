346bd667f51f6ad273d65190ac685e1f659eac70
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-23 07:44:52 -0300

    attt

--- src/c2wasm.js
+++ src/c2wasm.js
@@ -1,15 +1,14 @@
 let c2wasm = function(){
     let envcallbacks = {}
     let cfunctions = {}
+
+
+
+
     let main_obj = {
         envcallbacks: envcallbacks,
         cfunctions: cfunctions
     } 
+    return main_obj;
 
-//srcjs/teste.js
-
-main_obj.test = function() {
-    console.log("test");
-    return "test";
-}
-return main_obj;}()
+}()
