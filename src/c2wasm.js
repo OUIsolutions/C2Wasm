19fe8ba22e2f924eeda2253d4c4f1b0e9c52d695
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-23 15:19:01 -0300

    att

--- src/c2wasm.js
+++ src/c2wasm.js
@@ -1,24 +1,25 @@
 let c2wasm = function(){
 
     let main_module = {}
-    main_module.createEnv = function(stack) {
-
+    main_module.createEnv = function(stack,cfuncs) {
         let env_obj = {}
         
 
         return env_obj;
     } 
-    main_module.loadBytes = async function(wasmBytes, callback) {
+    main_module.loadBytes = async function(wasmBytes, startCCallback) {
 
         let created_obj = {};
+        let cfuncs = {};
 
-        
         created_obj.lib = {};
         created_obj.stack = [created_obj.lib];
         created_obj.importObject = {
-            env:main_module.createEnv(created_obj.stack)
+            env:main_module.createEnv(created_obj.stack,cfuncs)
         };
         created_obj.wasmModule = await WebAssembly.instantiate(wasmBytes, created_obj.importObject);
+        cfuncs.cfuncs = created_obj.wasmModule.instance.exports;
+        cfuncs.cfuncs[startCCallback](0);
         return created_obj;
     }
     main_module.loadModule = async function(module, callback) {
