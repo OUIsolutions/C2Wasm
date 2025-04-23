ad2c4420169b841d4d336764dea3ec491c2d16ae
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-23 14:59:23 -0300

    created start struct

--- build/js_creation.lua
+++ build/js_creation.lua
@@ -1,25 +1,5 @@
 
 
 function create_js_part()
-    local js_content = [[
-let c2wasm = function(){
-    let envcallbacks = {}
-    let cfunctions = {}
-    let main_obj = {
-        envcallbacks: envcallbacks,
-        cfunctions: cfunctions
-    } 
-
-]]
-    local files = darwin.dtw.list_files_recursively("srcjs",true)
-    for i=1,#files do
-        local file = files[i]
-        local content = darwin.dtw.load_file(file)
-        js_content = js_content.."//"..file.."\n"
-        js_content = js_content..content.."\n"
-    end
-    js_content = js_content.."return main_obj;"
-    js_content = js_content.."}()\n"
-
-    darwin.dtw.write_file("release/c2wasm.js",js_content)
+ 
 end 
--- src/c2wasm.js
+++ src/c2wasm.js
@@ -1,19 +1,26 @@
 let c2wasm = function(){
-    let envcallbacks = {}
-    let cfunctions = {}
-    let exports = null;
-    envcallbacks.set_function = function(name, func) {
-       console.log("set_function", name, func);
-    }
 
+    let main_module = {}
+    main_module.createEnv = function(stack,lib) {
 
+        let env_obj = {}
+        
 
-
-
-    let main_obj = {
-        envcallbacks: envcallbacks,
-        cfunctions: cfunctions
+        return env_obj;
     } 
-    return main_obj;
+    main_module.loadBytes = async function(wasmBytes, callback) {
+
+        let created_obj = {};
+        created_obj.lib = {};
+        created_obj.stack = [];
+        created_obj.env =  main_module.createEnv(created_obj.stack,created_obj.lib);
+        created_obj.wasmModule = await WebAssembly.instantiate(wasmBytes, created_obj.env);
+        return created_obj;
+    }
+    main_module.loadModule = async function(module, callback) {
+        let wasmBytes = await fetch(module).then(res => res.arrayBuffer());
+        return main_module.loadBytes(wasmBytes, callback);
+    }
+    return main_module;
 
 }()
