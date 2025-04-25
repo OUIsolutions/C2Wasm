ac55e0b2c71dcb63e8086e548a676d7df33fa6cc
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-25 06:39:13 -0300

    att

--- src/c2WasmVar.h
+++ src/c2WasmVar.h
@@ -6,3 +6,9 @@ typedef struct C2wasmVar {
 } C2wasmVar;
 
 
+EMSCRIPTEN_KEEPALIVE long  C2wasmVar_get_stack_index(C2wasmVar *js_var) {
+  return js_var->stack_index;
+}
+EMSCRIPTEN_KEEPALIVE long  C2wasmVar_get_internal_stack_index(C2wasmVar *js_var) {
+  return js_var->internal_stack_index;
+}
