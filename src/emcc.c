987f1e6cb5cab82e0715d1d20f1fe59a92fdfa64
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-25 06:35:02 -0300

    att

--- src/emcc.c
+++ src/emcc.c
@@ -4,10 +4,17 @@
 
 C2wasmVar c2wasm_window = {0, 0};
 
+
+
+
 EM_JS(void ,c2wasm_start_stack, (void), {
  
    if(window['c2wasm_stack'] == undefined){
       window['c2wasm_stack'] = [[window]];
    }
+});
 
+
+EM_JS(void ,c2wasm_set_int_prop,(C2wasmVar *js_var,const char *prop, int value), {
+   console.log(js_var);
 });
