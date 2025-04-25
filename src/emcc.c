78d1a898df80e46606d0f3ff124668b5dc500e83
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-25 05:58:40 -0300

    att

--- src/emcc.c
+++ src/emcc.c
@@ -1,5 +1,8 @@
 #include  "emcc_exports.h"
+#include "var.h"
 
+
+C2wasmVar c2wasm_window = {0, 0};
 
 EM_JS(void ,c2wasm_start_stack, (void), {
  
---
+++ src/var.h
@@ -0,0 +1,5 @@
+
+typedef struct C2wasmVar {
+  long  stack_index;
+  long internal_stack_index;
+} C2wasmVar;
