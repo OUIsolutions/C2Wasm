9ccf989c96b550c52f4c26d7dfdc9bf4446f1b3d
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-25 06:55:49 -0300

    att

--- src/emcc.c
+++ src/emcc.c
@@ -14,10 +14,10 @@ EM_JS(void ,c2wasm_start_stack, (void), {
 });
 
 
-EM_JS(void ,c2wasm_set_int_prop,(C2wasmVar *js_var,const char *prop, int value), {
+EM_JS(void ,c2wasm_set_int_prop,(C2wasmVar *js_var,const char *prop_name, int value), {
     let stack_index = wasmExports.C2wasmVar_get_stack_index(js_var);
     let internal_stack_index = wasmExports.C2wasmVar_get_internal_stack_index(js_var);
     let object = window['c2wasm_stack'][stack_index][internal_stack_index];
-    let prop_name = UTF8ToString(prop);
-    object[prop_name] = value;
+    let prop_name_formatted = UTF8ToString(prop_name);
+    object[prop_name_formatted] = value;
 });
