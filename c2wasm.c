7cb907f95712581422a9a4b351945cd0759e3b84
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-28 22:51:22 -0300

    attt

--- c2wasm.c
+++ c2wasm.c
@@ -67,31 +67,31 @@ EM_JS(void ,c2wasm_start, (void), {
 });
 //==================================JS Creation=============================================================
 
-EM_JS(void,c2wasm_create_long,(long value),{
+EM_JS(long,c2wasm_create_long,(long value),{
     let index = window.c2wasm_stack.length;
     window.c2wasm_stack.push(value);
     return index;
 });
 
-EM_JS(void,c2wasm_create_double,(double value),{
+EM_JS(long,c2wasm_create_double,(double value),{
     let index = window.c2wasm_stack.length;
     window.c2wasm_stack.push(value);
     return index;
 });
 
-EM_JS(void,c2wasm_create_object,(void),{
+EM_JS(long,c2wasm_create_object,(void),{
     let index = window.c2wasm_stack.length;
     window.c2wasm_stack.push({});
     return index;
 });
 
-EM_JS(void,c2wasm_create_array,(void),{
+EM_JS(long,c2wasm_create_array,(void),{
     let index = window.c2wasm_stack.length;
     window.c2wasm_stack.push([]);
     return index;
 });
 
-EM_JS(void,c2wasm_create_string,(const char *value),{
+EM_JS(long,c2wasm_create_string,(const char *value),{
     let index = window.c2wasm_stack.length;
     window.c2wasm_stack.push(value);
     return index;
