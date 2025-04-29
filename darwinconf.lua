42e2db387ef2320b04d9516a7561c6452f8bdf86
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-29 07:10:06 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -9,5 +9,10 @@ local MAX_CONNTENT = darwin.camalgamator.ONE_MB * 20
 local MAX_RECURSION = 100
 
 local onefile = darwin.camalgamator.generate_amalgamation("src/one.c", MAX_CONNTENT, MAX_RECURSION)
+onefile = string.gsub(onefile,"//silver_chain_scope_start","")
+onefile = string.gsub(onefile,"//DONT MODIFY THIS COMMENT","")
+onefile = string.gsub(onefile,"//this import is computationally generated","")
+onefile = string.gsub(onefile,"//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain","")
+onefile = string.gsub(onefile,"//silver_chain_scope_end","")
 
 darwin.dtw.write_file("c2wasm.c",onefile)
