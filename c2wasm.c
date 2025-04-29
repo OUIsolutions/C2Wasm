249130043fbfb571aef5111c9b5a1adf48504d1e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-29 02:49:03 -0300

    att

--- c2wasm.c
+++ c2wasm.c
@@ -273,6 +273,21 @@ EM_JS(int,c2wasm_is_object_prop_null,(long stack_index, const char *prop_name),{
     let prop_name_formatted = window.c2wasm_get_string(prop_name);
     return object[prop_name_formatted] == null;
 })
+
+EM_JS(long,c2wasm_call_object_prop,(long stack_index, const char *prop_name,long args),{
+    let object = window.c2wasm_stack[stack_index];
+    let prop_name_formatted = window.c2wasm_get_string(prop_name);
+    let arguments = undefined;
+    if(args != -1){
+        arguments = window.c2wasm_stack[args];
+    }
+    long result = object[prop_name_formatted](arguments);
+    let index = window.c2wasm_stack.length;
+    window.c2wasm_stack.push(result);
+    return index;
+})
+
+
 //==================================JS Object Seters ========================================================
 
 EM_JS(void ,c2wasm_set_object_prop_long,(long stack_index, const char *prop_name, long value), {
