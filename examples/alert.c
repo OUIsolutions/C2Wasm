4ee12846613f3eb9de112eee200faaabdc756fd6
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-29 04:20:50 -0300

    att

---
+++ examples/alert.c
@@ -0,0 +1,10 @@
+#include "c2wasm.c"
+
+
+
+int main(){
+  c2wasm_start();
+  long alert_args = c2wasm_create_array();
+  c2wasm_append_array_string(alert_args,"hello from c");
+  c2wasm_call_object_prop(c2wasm_window,"alert",alert_args);
+}
---
+++ examples/setting_value_in_div.c
@@ -0,0 +1,12 @@
+#include "c2wasm.c"
+
+
+
+int main(){
+  c2wasm_start();
+  long find_args = c2wasm_create_array();
+  c2wasm_append_array_string(find_args,"test_div");
+  long test_div = c2wasm_call_object_prop(c2wasm_document,"getElementById",find_args);
+  c2wasm_set_object_prop_string(test_div,"innerHTML","Hello World setted in C");
+
+}
