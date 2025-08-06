80ab9a044fae5b2c8c6b106b64863d85e6fc0dc4
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-06 06:29:03 -0300

    created new call methods

--- src/em_js.js_vars.c
+++ src/em_js.js_vars.c
@@ -86,4 +86,80 @@ EM_JS(c2wasm_js_var, c2wasm_get_var_any, (c2wasm_js_var stack_index), {
     return created_index;
 });
 
+EM_JS(c2wasm_js_var, c2wasm_call_var, (c2wasm_js_var stack_index, c2wasm_js_var args), {
+    let func = window.c2wasm_stack[stack_index];
+    let arguments_callback = null;
+    if(args < 0){
+        arguments_callback = [];
+    }
+    else{
+        arguments_callback = window.c2wasm_stack[args];
+        if(!Array.isArray(arguments_callback)){
+            arguments_callback = [arguments_callback];
+        }
+    }
+    let result = null;
+    try{
+        result = func(...arguments_callback);
+    }catch(error){
+        result = error;
+    }
+
+    if(result == false){
+        return window.c2wasm_false;
+    }
+    if(result == true){
+        return window.c2wasm_true;
+    }
+    if(result == null){
+        return window.c2wasm_null;
+    }
+    if(result == undefined){
+        return window.c2wasm_undefined;
+    }
+    let created_index = window.c2wasm_get_stack_point();
+    window.c2wasm_stack[created_index] = result;
+    return created_index;
+});
+
+#ifdef __ASYNCIFY__
+
+EM_ASYNC_JS(c2wasm_js_var, await_c2wasm_call_var, (c2wasm_js_var stack_index, c2wasm_js_var args), {
+    let func = window.c2wasm_stack[stack_index];
+    let arguments_callback = null;
+    if(args < 0){
+        arguments_callback = [];
+    }
+    else{
+        arguments_callback = window.c2wasm_stack[args];
+        if(!Array.isArray(arguments_callback)){
+            arguments_callback = [arguments_callback];
+        }
+    }
+    let result = null;
+    try{
+        result = await func(...arguments_callback);
+    }catch(error){
+        result = error;
+    }
+
+    if(result == false){
+        return window.c2wasm_false;
+    }
+    if(result == true){
+        return window.c2wasm_true;
+    }
+    if(result == null){
+        return window.c2wasm_null;
+    }
+    if(result == undefined){
+        return window.c2wasm_undefined;
+    }
+    let created_index = window.c2wasm_get_stack_point();
+    window.c2wasm_stack[created_index] = result;
+    return created_index;
+});
+
+#endif // __ASYNCIFY__
+
 
