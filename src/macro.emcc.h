67518134d20c6df66e5c332dfa79a6e61d2734e5
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-06 06:26:52 -0300

    att

--- src/macro.emcc.h
+++ src/macro.emcc.h
@@ -1,16 +1,23 @@
-
+#ifndef EMSCRIPTEN_KEEPALIVE
+#define EMSCRIPTEN_KEEPALIVE
+#endif
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
-#ifndef EMSCRIPTEN_KEEPALIVE
-#define EMSCRIPTEN_KEEPALIVE
-#endif
+
 
 #ifndef EM_JS
 #define EM_JS(ret, name, params, body) ret name params;
 #endif
+#ifdef __ASYNCIFY__
+
+#ifndef EM_ASYNC_JS
+#define EM_ASYNC_JS(ret, name, params, body) ret name params;
+#endif
+#endif 
+
 #ifndef true 
 #define true 1
 #endif
