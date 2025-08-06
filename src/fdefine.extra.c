e3d8ca1d5b06336337e6f5dac46dd4a858b99c41
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-05 23:13:13 -0300

    added strlen

--- src/fdefine.extra.c
+++ src/fdefine.extra.c
@@ -4,7 +4,13 @@
 //silver_chain_scope_end
 
 
-
+long private_emcc_strlen(const char *str) {
+    long len = 0;
+    while(str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
 
 
 EMSCRIPTEN_KEEPALIVE char c2wasm_get_char(const char *str,int index) {
@@ -14,7 +20,7 @@ EMSCRIPTEN_KEEPALIVE void c2wasm_set_char(char *str,int index,char value) {
     str[index] = value;
 }
 EMSCRIPTEN_KEEPALIVE int c2wasm_get_str_size(const char *str) {
-    return strlen(str);
+    return private_emcc_strlen(str);
 }
 
 
