53da5dddd39983acb89daf53b96b441f611b80ec
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-06 06:16:34 -0300

    att

--- src/object/em_js.object_get.c
+++ src/object/em_js.object_get.c
@@ -103,50 +103,22 @@ EM_JS(c2wasm_js_var , c2wasm_get_object_prop_any,(c2wasm_js_var stack_index, con
 EM_JS(c2wasm_js_var,c2wasm_call_object_prop,(c2wasm_js_var stack_index, const char *prop_name,c2wasm_js_var args),{
     let object = window.c2wasm_stack[stack_index];
     let prop_name_formatted = window.c2wasm_get_string(prop_name);
-    let arguments = undefined;
-    if(args != -1){
-        arguments = window.c2wasm_stack[args];
+    let arguments_callback = null;
+    if(args < 0){
+        arguments_callback = [];
+    }
+    else{
+        arguments_callback = window.c2wasm_stack[args];
+        if(!Array.isArray(arguments_callback)){
+            arguments_callback = [arguments_callback];
+        }
     }
     let result = null;
     try{
-        result = object[prop_name_formatted](...arguments);
+        result = object[prop_name_formatted](...arguments_callback);
     }catch(error){
         result = error;
     }
-
-    if(result == false){
-        return  window.c2wasm_false;
-    }
-    if(result == true){
-        return window.c2wasm_true;
-    }
-    if(result == null){
-        return window.c2wasm_null;
-    }
-    if(result == undefined){
-        return window.c2wasm_undefined;
-    }
-    let created_index = window.c2wasm_get_stack_point();
-    window.c2wasm_stack[created_index] = result;
-    return created_index;
-});
-
-EM_JS(c2wasm_js_var,async_c2wasm_call_object_prop,(c2wasm_js_var stack_index, const char *prop_name,c2wasm_js_var args),{
-    let object = window.c2wasm_stack[stack_index];
-    let prop_name_formatted = window.c2wasm_get_string(prop_name);
-    let arguments = undefined;
-    if(args != -1){
-        arguments = window.c2wasm_stack[args];
-    }
-    let result = null;
-   
-    (async function(){
-        try{
-            result = await object[prop_name_formatted](...arguments);
-        }catch(error){
-            result = error;
-        }     
-    })();
 
     if(result == false){
         return  window.c2wasm_false;
