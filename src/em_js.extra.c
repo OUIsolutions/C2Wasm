815c20caf43f194cd6aeac753e122e9f81f586d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-11-07 07:15:56 -0300

    memcpy copy

--- src/em_js.extra.c
+++ src/em_js.extra.c
@@ -4,18 +4,22 @@
 //silver_chain_scope_end
 
 EM_JS(long ,c2wasm_get_string_len, (c2wasm_js_var string_index), {
-    return window.c2wasm_stack[string_index].length;
+    let value = window.c2wasm_stack[string_index];
+    let encoder = new TextEncoder();
+    let utf8Array = encoder.encode(value);
+    return utf8Array.length;
 });
 
 EM_JS(void *,c2wasm_memcpy_string,(c2wasm_js_var stack_index,int start_string, char *dest, int size), {
     let value = window.c2wasm_stack[stack_index];
-    for(let i = 0; i < size; i++){
-      
-        let current_char = value.charCodeAt(i+start_string);
-        if(isNaN(current_char)){
+    let encoder = new TextEncoder();
+    let utf8Array = encoder.encode(value);
+    
+    for(let i = 0; i <= size; i++){
+        if(i + start_string >= utf8Array.length){
             break;
         }
-        wasmExports.c2wasm_set_char(dest,i,current_char);
+        wasmExports.c2wasm_set_char(dest, i, utf8Array[i + start_string]);
     }
     return dest;
 });
